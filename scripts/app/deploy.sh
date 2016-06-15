cd8a42c8601f839ecf36a126e95252f4d2a094df
root <root@precise64.(none)>
2016-06-15 01:04:20 +0000

    copy artifect

--- scripts/app/deploy.sh
+++ scripts/app/deploy.sh
@@ -1,16 +1,20 @@
 #!/bin/bash 
 
 
-BUILD=`md5sum /opt/apps/jenkins_home/jobs/Build/lastSuccessful/archive/target/helloproject.war|awk -F" " '{print $1}'`
-DEPLOY=`md5sum ${TOMCAT_HOME}/webapps/helloproject.war|awk -F" " '{print $1 }'`
+BUILD=`md5sum ${WORKSPACE}/packages/target/helloproject.war|awk -F" " '{print $1}'`
 
-if [ ${BUILD} != ${DEPLOY} ] 
+if [ -f ${TOMCAT_HOME}/webapps/helloproject.war ]
+then
+DEPLOY=`md5sum ${TOMCAT_HOME}/webapps/helloproject.war|awk -F" " '{print $1 }'`
+fi
+
+if [ ! -f ${TOMCAT_HOME}/webapps/helloproject.war ] || [ ${BUILD} != ${DEPLOY} ]
 then
 # Remove old war file 
 rm -rf ${TOMCAT_HOME}/webapps/helloproject*
 
 # Update latest war 
-cp -r /opt/apps/jenkins_home/jobs/Build/lastSuccessful/archive/target/helloproject.war ${TOMCAT_HOME}/webapps/
+cp -r ${WORKSPACE}/packages/target/helloproject.war  ${TOMCAT_HOME}/webapps/
 
 # Restart tomcat 
 cd ${TOMCAT_HOME}/bin/ && BUILD_ID=DoNotKill && ./shutdown.sh
